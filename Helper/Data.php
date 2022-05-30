8e83bb68d05c6e2750967db400870059e56de1d2
Siarhei Uchukhlebau <s.uchuhlebov@mageworx.com>
2022-05-31 01:13:13 +0400

    Block reauthorization when old sales processor in use

--- Helper/Data.php
+++ Helper/Data.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 namespace MageWorx\OrderEditorBraintree\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use MageWorx\OrderEditor\Helper\Data as OrderEditorHelper;
 
 class Data extends AbstractHelper
 {
@@ -15,10 +17,28 @@ class Data extends AbstractHelper
         'mageworx_order_management/order_editor/invoice_shipment_refund/enable_braintree_reauthorization';
 
     /**
+     * @var OrderEditorHelper
+     */
+    protected $orderEditorHelper;
+
+    /**
+     * @param Context $context
+     * @param OrderEditorHelper $orderEditorHelper
+     */
+    public function __construct(
+        Context           $context,
+        OrderEditorHelper $orderEditorHelper
+    ) {
+        $this->orderEditorHelper = $orderEditorHelper;
+        parent::__construct($context);
+    }
+
+    /**
      * @return bool
      */
     public function isEnabled(): bool
     {
-        return $this->scopeConfig->isSetFlag(static::XML_PATH_IS_ENABLED);
+        return $this->scopeConfig->isSetFlag(static::XML_PATH_IS_ENABLED)
+            && $this->orderEditorHelper->isReauthorizationAllowed();
     }
 }
